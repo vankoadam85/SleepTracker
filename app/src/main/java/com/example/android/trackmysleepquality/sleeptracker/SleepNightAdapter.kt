7f56889146c03b7d0f6c587522e2cde89b62fe51
Vankó Ádám <vankoadam85@gmail.com>
2021-02-19 18:40:13 +0100

    implement SleepNightAdapter.onCreateViewHolder()

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,15 +1,23 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.TextItemViewHolder>() {
-    private val data = listOf<SleepNight>()
+class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        TODO("Not yet implemented")
+        val textView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(textView)
     }
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
@@ -24,5 +32,5 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.TextItemViewHold
      * A ViewHolder holds a view for the [RecyclerView] as well as providing additional information
      * to the RecyclerView such as where on the screen it was last drawn during scrolling.
      */
-    class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+    class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
