3781738354b9b1f77ec3d6aa3b9a5788b71b7004
Vankó Ádám <vankoadam85@gmail.com>
2021-02-19 14:27:06 +0100

    add SleepNightAdapter

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.TextItemViewHolder>() {
+    private val data = listOf<SleepNight>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        holder.textView.text = data[position].sleepQuality.toString()
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    /**
+     * ViewHolder that holds a single [TextView].
+     *
+     * A ViewHolder holds a view for the [RecyclerView] as well as providing additional information
+     * to the RecyclerView such as where on the screen it was last drawn during scrolling.
+     */
+    class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+}
